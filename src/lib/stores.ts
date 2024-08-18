9d8243f647edbba443744162096afa9943d2af97
YeshwanthSivaraj <mail2yeshwanthsivaraj@gmail.com>
2024-08-18 20:02:20 +0530

    added cart store and functions

---
+++ src/lib/stores.ts
@@ -0,0 +1,60 @@
+import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
+
+export interface CartItem {
+	id: string;
+	title: string;
+	price: number;
+	image: string;
+	quantity: number;
+}
+
+const cartItemsStore = writable(getStorePersistance('profileFyiStore'));
+
+cartItemsStore.subscribe((val) => {
+	console.log('val: ', val);
+	if (browser) {
+		localStorage.setItem('profileFyiStore', JSON.stringify(val));
+	}
+});
+
+function getStorePersistance(key: string): CartItem[] {
+	if (!browser) {
+		return [] as CartItem[];
+	}
+
+	const itemsStored: CartItem[] = JSON.parse(localStorage.getItem(key) || '{}');
+
+	console.log('items stored: ', itemsStored);
+
+	if (!itemsStored.length) {
+		return [] as CartItem[];
+	}
+
+	return itemsStored;
+}
+
+export function addToCart(newItem: CartItem) {
+	cartItemsStore.update((cartItems) => {
+		const foundItem = cartItems.findIndex((item) => item.title === newItem.title);
+
+		if (foundItem !== -1) {
+			cartItems[foundItem].quantity += newItem.quantity;
+			return cartItems;
+		}
+
+		return [...cartItems, newItem];
+	});
+}
+
+export function removeFromCart(title: string) {
+	cartItemsStore.update((cartItems) => {
+		const items = cartItems.filter((item) => item.title !== title);
+
+		return items;
+	});
+}
+
+export function emptyCart() {
+	cartItemsStore.set([]);
+}
