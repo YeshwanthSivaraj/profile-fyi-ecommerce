8391d9ed69f59b4c47442c0fee79dd6798ad14fb
YeshwanthSivaraj <mail2yeshwanthsivaraj@gmail.com>
2024-08-18 19:28:14 +0530

    added product details route

---
+++ src/routes/products/[slug]/+page.server.ts
@@ -0,0 +1,25 @@
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+const baseUrl = 'https://fakestoreapi.com';
+
+export const load = (async ({ params }) => {
+	try {
+		const productData = await fetch(`${baseUrl}/products/${params.slug}`);
+		const product = await productData.json();
+
+		const relatedProductsData = await fetch(`${baseUrl}/products/category/${product.category}`);
+		const relatedProducts = await relatedProductsData.json();
+
+		return {
+			product,
+			relatedProducts
+		};
+	} catch (e) {
+		console.error(`Couldnt load page /${params.slug}`);
+
+		throw error(500, {
+			message: 'Couldnt load this page'
+		});
+	}
+}) satisfies PageServerLoad;
---
+++ src/routes/products/[slug]/+page.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+	import ProductItem from '$lib/ProductItem.svelte';
+	import ProductQuantity from '$lib/ProductQuantity.svelte';
+
+	export let data: any;
+
+	$: product = data.product;
+	$: relatedProducts = data.relatedProducts;
+	let quantity = 1;
+
+	function addItemToCart() {}
+</script>
+
+<svelte:head>
+	<title>{product.title} | Profile fyi Ecommerce</title>
+</svelte:head>
+
+<div class="flex flex-col mx-3 py-10 gap-5 lg:mx-10 lg:flex-row lg:gap-8">
+	<div class="basis-1/2 px-8 flex">
+		<img
+			class="object-contain aspect-square"
+			src={product.image}
+			width="700"
+			height="700"
+			loading="lazy"
+			alt="{product.title} image"
+		/>
+	</div>
+	<div class="basis-1/2 flex flex-col gap-8 px-8">
+		<h1 class="font-extrabold text-4xl mt-3">{product.title}</h1>
+		<div class="flex items-end gap-2">
+			<span class="text-3xl">₹{product.price}</span>
+		</div>
+
+		<div class="flex flex-col gap-2">
+			<p>Description</p>
+			<p class="text-gray-600">
+				{product.description}
+			</p>
+		</div>
+
+		<div class="flex flex-col gap-2">
+			<p>Quantity</p>
+			<ProductQuantity bind:count={quantity} />
+		</div>
+		<button
+			class="w-full h-12 px-6 text-black font-bold transition-colors duration-150 bg-yellow-300 focus:shadow hover:bg-yellow-500"
+			on:click={() => addItemToCart()}
+		>
+			Add to cart
+		</button>
+	</div>
+</div>
+
+<div class="flex flex-col px-10">
+	<div>
+		<p class="font-medium text-xl uppercase text-center">Related</p>
+	</div>
+
+	<div class="grid gap-12 pr-5 py-5 grid-cols-2 md:grid-cols-3">
+		{#each relatedProducts as relatedProduct}
+			<ProductItem
+				title={relatedProduct.title}
+				image={relatedProduct.image}
+				price={relatedProduct.price}
+				link="/products/{relatedProduct.id}"
+			/>
+		{/each}
+	</div>
+</div>
